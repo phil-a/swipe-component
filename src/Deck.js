ca279218565fccb9e63bcd0c6264d1589af9b9f3
Philip Alekseev <phil.alekseev@gmail.com>
2017-04-18 21:12:52 -0400

    Add rotation interpolation to animation

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { View, Animated, PanResponder } from "react-native";
+import { View, Animated, PanResponder, Dimensions } from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 class Deck extends Component {
   constructor(props) {
@@ -14,28 +16,55 @@ class Deck extends Component {
         position.setValue({ x: gesture.dx, y: gesture.dy });
       },
 
-      onPanResponderRelease: (event, gesture) => {
-        console.log(gesture);
+      onPanResponderRelease: () => {
+        this.resetPosition();
       },
     });
 
     this.state = { panResponder, position };
   }
 
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 },
+    }).start();
+  }
+
+  getCardStyle() {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ["-120deg", "0deg", "120deg"],
+    });
+
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }],
+    };
+  }
+
   renderCards() {
-    return this.props.data.map(item => {
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={this.getCardStyle()}
+            {...this.state.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
       return this.props.renderCard(item);
     });
   }
 
   render() {
     return (
-      <Animated.View
-        style={this.state.position.getLayout()}
-        {...this.state.panResponder.panHandlers}
-      >
+      <View>
         {this.renderCards()}
-      </Animated.View>
+      </View>
     );
   }
 }
