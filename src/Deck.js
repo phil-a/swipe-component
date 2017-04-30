773e32cdc57e25b4ce6f193bfd2ff49dea913c5f
Philip Alekseev <phil.alekseev@gmail.com>
2017-04-30 13:04:32 -0400

    Attach panhandlers to current card

--- src/Deck.js
+++ src/Deck.js
@@ -57,6 +57,8 @@ class Deck extends Component {
     const item = data[this.state.index];
 
     direction === "right" ? onSwipeRight(item) : onSwipeLeft(item);
+    this.state.position.setValue({ x: 0, y: 0 });
+    this.setState({ index: this.state.index + 1 });
   }
 
   getCardStyle() {
@@ -73,8 +75,12 @@ class Deck extends Component {
   }
 
   renderCards() {
-    return this.props.data.map((item, index) => {
-      if (index === 0) {
+    return this.props.data.map((item, itemIndex) => {
+      if (itemIndex < this.state.index) {
+        return null;
+      }
+
+      if (itemIndex === this.state.index) {
         return (
           <Animated.View
             key={item.id}
